cae98e4f0ad3acfda70b3f8b69645b6bd0a0b940
Kemosalamy <karimosalamy@gmail.com>
2021-04-23 16:59:25 +0200

    finished Building class and started EconomicBuilding

--- TEAM47CSEN401Game/src/buildings/Building.java
+++ TEAM47CSEN401Game/src/buildings/Building.java
@@ -1,5 +1,45 @@
 package buildings;
 
 public class Building {
+	private int cost;
+	private int level;
+	private int upgradeCost;
+	private boolean coolDown;
+	
+	public Building(int cost, int upgradeCost) {
+		this.cost = cost;
+		this.level = 1;
+		this.upgradeCost = upgradeCost;
+		this.coolDown = true;
+	}
+	
+	public int getCost() {
+		return this.getCost();
+	}
+	
+	public int getLevel() {
+		return this.level;
+	}
+	
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	public int getUpgradeCost() {
+		return this.upgradeCost;
+	}
+	
+	public void setUpgradeCost(int upgradeCost) {
+		this.upgradeCost = upgradeCost;
+	}
+	
+	public boolean getCoolDown() {
+		return this.coolDown;
+	}
+	
+	public void setCoolDown(boolean coolDown) {
+		this.coolDown = coolDown;
+	}
+	
 
 }
