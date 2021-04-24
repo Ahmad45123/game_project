30f774224d169f13ff8fc34bbcc2a7f7fa5522e0
Kemosalamy <karimosalamy@gmail.com>
2021-04-24 14:27:19 +0200

    implemented loadCitiesAndDistances function which loads csv file

--- README.md
+++ README.md
@@ -2,5 +2,4 @@ team 47 group game
 
 WHERE I LEFT OFF(If someone wants to continue this is where you should continue from):
 
-Game class. Implemented csv loader in the loadCitiesAndDistances function however it does not work or do anything. On my machine it 
-throws a "FileNotFoundException" for some reason. try on your machines.
+Game class. loadArmy Function is still not implemented, loadCitiesAndDistances is fully functional.
--- TEAM47CSEN401Game/src/engine/Game.java
+++ TEAM47CSEN401Game/src/engine/Game.java
@@ -35,19 +35,34 @@ public class Game {
         this.currentTurnCount = currentTurnCount;
     }
     
+    
+    //no clue what this actually does, the description is very annoyingly vague
     public void loadArmy(String cityName, String path) throws IOException{
     	
     }
     
-    //Taken from the how to read CSV PDF in CMS
-    //Function doesn't work for some reason???
-    //On my eclipse it states that it cannot find the distances.csv file
+    
+    // there are 2 distances.csv in the folder structure because eclipse wont make this function work without it... so just leave them as is
     //CSV is not my favorite file format
     private void loadCitiesAndDistances() throws IOException{
     	String currentLine = "";
     	FileReader fileReader = new FileReader("distances.csv");
-    	BufferedReader br = new BufferedReader(new FileReader("distances.csv"));
+    	BufferedReader br = new BufferedReader(fileReader);
     	while((currentLine = br.readLine()) != null) {
+    		//Splits every line in the CSV into a string array of size 3, string[0] is cityname1, string[1] is cityname2 and string[2] is the distance between them
+    		//makes a distance object and adds it to the ArrayList, also adds the cities to the availableCities ArrayList if they are not found within it already
+    		String lineDistanceData[] = currentLine.split(",");
+    		Distance currentLineDistance = new Distance(lineDistanceData[0],lineDistanceData[1],Integer.parseInt(lineDistanceData[2]));
+    		distances.add(currentLineDistance);
+    		
+    		City first = new City(lineDistanceData[0]);
+    		City second  = new City(lineDistanceData[1]);
+    		if(!availableCities.contains(first)) {
+    			availableCities.add(first);
+    		}
+    		if(!availableCities.contains(second)) {
+    			availableCities.add(second);
+    		}	
     	}
     	br.close();
     }
@@ -59,10 +74,9 @@ public class Game {
         this.maxTurnCount = 30;
         this.currentTurnCount = 1;
         this.player = new Player(playerName);
-        // Load Cities
+        
+        // Load Cities And Distances
         loadCitiesAndDistances();
-    }
-   
-    
+    }    
     
 }
