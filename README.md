b01812af8c7dbd0e901b6b6fef2cb1a2b1fdad86
Kemosalamy <karimosalamy@gmail.com>
2021-04-24 15:47:18 +0200

    added csv reading and implementation for the loadArmy function

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ team 47 group game
 
 WHERE I LEFT OFF(If someone wants to continue this is where you should continue from):
 
-Game class. loadArmy Function is still not implemented, loadCitiesAndDistances is fully functional.
+Exceptions unit, start from the very first Exception class: EmpireException
--- TEAM47CSEN401Game/src/engine/Game.java
+++ TEAM47CSEN401Game/src/engine/Game.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import units.*;
 
 
 public class Game {
@@ -36,9 +37,55 @@ public class Game {
     }
     
     
-    //no clue what this actually does, the description is very annoyingly vague
+    //I really hate CSV and Java
+    //Here I am looping through all the available cities to find the one that matches the argument name(because java)
+    //then I create a new army and units ArrayList and fill them with the information I extracted from the .CSV file
+    //finally I put the unit in the army, and then pass the new army I just created back to the original city I was working on
+    //I really hope this works based on references 
     public void loadArmy(String cityName, String path) throws IOException{
-    	
+    	for(City c : availableCities) {
+    		if(c.getName().equals(cityName)) {
+    			Army cityArmy = new Army(cityName);
+    			ArrayList<Unit> armyUnits = new ArrayList<Unit>();
+    			String currentLine = "";
+    			FileReader fileReader = new FileReader(path);
+    			BufferedReader br = new BufferedReader(fileReader);
+    			while((currentLine = br.readLine()) != null) {
+    				String currentUnitLine[] = currentLine.split(",");
+    				switch(currentUnitLine[0]) {
+    				case "Archer":
+    					Archer currentArcher;
+    					int currentArcherLevel = Integer.parseInt(currentUnitLine[1]);
+    					if(currentArcherLevel==1)         currentArcher = new Archer(currentArcherLevel,60,0.4,0.5,0.6);
+    					else if(currentArcherLevel==2)    currentArcher = new Archer(currentArcherLevel,60,0.4,0.5,0.6);
+    					else                              currentArcher = new Archer(currentArcherLevel,70,0.5,0.6,0.7);
+    					armyUnits.add(currentArcher);
+    					break;
+    			
+    				case "Infantry":
+    					Infantry currentInfantry;
+    					int currentInfantryLevel = Integer.parseInt(currentUnitLine[1]);
+    					if(currentInfantryLevel==1)       currentInfantry = new Infantry(currentInfantryLevel,50,0.5,0.6,0.7);
+    					else if(currentInfantryLevel==2)  currentInfantry = new Infantry(currentInfantryLevel,50,0.5,0.6,0.7);
+    					else                              currentInfantry = new Infantry(currentInfantryLevel,60,0.6,0.7,0.8);
+    					armyUnits.add(currentInfantry);
+    					break;
+    					
+    				case "Cavalry":
+    					Cavalry currentCavalry;
+    					int currentCavalryLevel = Integer.parseInt(currentUnitLine[1]);
+    					if(currentCavalryLevel==1)         currentCavalry = new Cavalry(currentCavalryLevel,40,0.6,0.7,0.75);
+    					else if(currentCavalryLevel==2)    currentCavalry = new Cavalry(currentCavalryLevel,40,0.6,0.6,0.75);
+    					else                               currentCavalry = new Cavalry(currentCavalryLevel,60,0.7,0.8,0.9);
+    					armyUnits.add(currentCavalry);
+    					break;
+    				}
+    			}
+    			br.close();
+    			cityArmy.setUnits(armyUnits);
+    			c.setDefendingArmy(cityArmy);
+    		}
+    	}
     }
     
     
@@ -55,13 +102,21 @@ public class Game {
     		Distance currentLineDistance = new Distance(lineDistanceData[0],lineDistanceData[1],Integer.parseInt(lineDistanceData[2]));
     		distances.add(currentLineDistance);
     		
-    		City first = new City(lineDistanceData[0]);
-    		City second  = new City(lineDistanceData[1]);
-    		if(!availableCities.contains(first)) {
-    			availableCities.add(first);
+    		boolean firstCityExistCheck = true;
+    		boolean secondCityExistCheck  = true;
+    		
+    		for(City c : availableCities) {
+    			if(c.getName().equals(lineDistanceData[0])) firstCityExistCheck = false;
+    			if(c.getName().equals(lineDistanceData[1])) secondCityExistCheck = false;
     		}
-    		if(!availableCities.contains(second)) {
-    			availableCities.add(second);
+    		
+    		if(firstCityExistCheck==true) {
+    			City firstArgumentCity = new City(lineDistanceData[0]);
+    			availableCities.add(firstArgumentCity);
+    		}
+    		if(secondCityExistCheck==true) {
+    			City secondArgumentCity  = new City(lineDistanceData[1]);
+    			availableCities.add(secondArgumentCity);
     		}	
     	}
     	br.close();
@@ -77,6 +132,11 @@ public class Game {
         
         // Load Cities And Distances
         loadCitiesAndDistances();
+        
+        //LoadArmies for non Player Armies
+        for(City c : availableCities) {
+        	if(!c.getName().equals(playerCity)) loadArmy(c.getName(),c.getName().toLowerCase()+"_army.csv");
+        }
     }    
     
 }
